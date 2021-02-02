9840dc866f5b68bf1af35405fdbbec9e600e988f
jonlima2018 <jonathan_martins_lima@yahoo.com.br>
2021-02-02 17:40:07 -0300

    feat: add GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import BoxShadow from './pages/BoxShadow';
-import './App.css';
 
 const App:React.FC = () => (
   <BoxShadow />
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyles from './GlobalStyles';
 import BoxShadowProvider from './context/BoxShadow/Provider';
 
 ReactDOM.render(
   <React.StrictMode>
     <BoxShadowProvider>
+      <GlobalStyles />
       <App />
     </BoxShadowProvider>
   </React.StrictMode>,
