2ce463008f3509fae442ab8f32f76e41771291a3
jonlima2018 <jonathan_martins_lima@yahoo.com.br>
2021-02-02 02:43:33 -0300

    fix(Slider): corrigido typescript

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -7,32 +7,34 @@ interface SlideProps {
   value: number
   min: number
   max: number
-  handleInputChange: any
-  handleChange: any
+  handleChange(value: number): void;
+
 }
 
-const Slide: React.FC<SlideProps> = ({ name, value, min, max, handleInputChange, handleChange }) => {
-  return (
-    <StyledSlider>
-      <div className="container__slider">
-        <span className="slider__title">{name}</span>
-        <Input
-          value={value}
-          margin="dense"
-          onChange={handleInputChange}
-          inputProps={{ step: 1, min, max, type: 'number' }}
-        />
-      </div>
-      <Slider
-        step={1}
-        min={min}
-        max={max}
+const Slide: React.FC<SlideProps> = ({
+  name, value, min, max, handleChange,
+}) => (
+  <StyledSlider>
+    <div className="container__slider">
+      <span className="slider__title">{name}</span>
+      <Input
         value={value}
-        onChange={handleChange}
-        valueLabelDisplay="auto"
+        margin="dense"
+        onChange={({ target }) => handleChange(Number(target.value))}
+        inputProps={{
+          step: 1, min, max, type: 'number',
+        }}
       />
-    </StyledSlider>
-  );
-};
+    </div>
+    <Slider
+      step={1}
+      min={min}
+      max={max}
+      value={value}
+      onChange={(_, values) => handleChange(Number(values))}
+      valueLabelDisplay="auto"
+    />
+  </StyledSlider>
+);
 
 export default Slide;
